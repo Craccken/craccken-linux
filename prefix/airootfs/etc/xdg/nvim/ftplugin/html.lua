087fcdf74fdcfb66d472fde3f5ba4eb1f19ea6b7
Xaxeric <xaxeric.dev@gmail.com>
2022-12-28 21:09:06 +0700

    build: refresh config
    new(neovim): filetype config for html
    add(zsh): a lot of environment variable of `gum` commands for styling

---
+++ prefix/airootfs/etc/xdg/nvim/ftplugin/html.lua
@@ -0,0 +1,3 @@
+vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
+vim.opt.tabstop = 2     -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2  -- Number of spaces to use for each step of (auto)indent
